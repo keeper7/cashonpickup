01b85139d632d5e0b95174a33fa7df406472df63
keeper7 <mail@me.com>
2020-02-14 13:33:37 +0100

    Config update

---
+++ translations/cs.php
@@ -0,0 +1,23 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cashonpickup}prestashop>cashonpickup_b960e397881843c8ecf334f2cd90d417'] = 'Platba v hotovosti';
+$_MODULE['<{cashonpickup}prestashop>cashonpickup_14e3f9369ac9887fc6b150a598250135'] = 'Přijímat platby v hotovosti';
+$_MODULE['<{cashonpickup}prestashop>validation_6ff063fbc860a79759a7369ac32cee22'] = 'Objednávka';
+$_MODULE['<{cashonpickup}prestashop>validation_e5ec65b5e3f973d7e941b0bf2e9c765a'] = 'Platba v hotovosti';
+$_MODULE['<{cashonpickup}prestashop>validation_f1d3b424cd68795ecaa552883759aceb'] = 'Shrnutí objednávky';
+$_MODULE['<{cashonpickup}prestashop>validation_656109f7cb225cc3d01e5e266311c86a'] = 'Vybrali jste metodu zaplacení v hotovosti.';
+$_MODULE['<{cashonpickup}prestashop>validation_e2867a925cba382f1436d1834bb52a1c'] = 'Celková hodnota objednávky činí';
+$_MODULE['<{cashonpickup}prestashop>validation_1f87346a16cf80c372065de3c54c86d9'] = '(s DPH)';
+$_MODULE['<{cashonpickup}prestashop>validation_52f64bc0164b0e79deaeaaaa7e93f98f'] = 'Prosím potvrďte objednávku kliknutím na tlačítko \"Potvrdit objednávku\"';
+$_MODULE['<{cashonpickup}prestashop>validation_569fd05bdafa1712c4f6be5b153b8418'] = 'Další způsoby platby';
+$_MODULE['<{cashonpickup}prestashop>validation_46b9e3665f187c739c55983f757ccda0'] = 'Potvrzuji objednávku';
+$_MODULE['<{cashonpickup}prestashop>confirmation_88526efe38fd18179a127024aba8c1d7'] = 'Vaše objednávka %s je kompletní.';
+$_MODULE['<{cashonpickup}prestashop>confirmation_656109f7cb225cc3d01e5e266311c86a'] = 'Vybrali jste metodu zaplacení v hotovosti.';
+$_MODULE['<{cashonpickup}prestashop>confirmation_e6dc7945b557a1cd949bea92dd58963e'] = 'Vaše objednávka bude připravena co nejdříve.';
+$_MODULE['<{cashonpickup}prestashop>confirmation_0db71da7150c27142eef9d22b843b4a9'] = 'Pro dotazy nebo informace kontaktujte prosím naši';
+$_MODULE['<{cashonpickup}prestashop>confirmation_64430ad2835be8ad60c59e7d44e4b0b1'] = 'zákaznickou podporu';
+$_MODULE['<{cashonpickup}prestashop>payment_1f40f6575eb0f4242dae480bcb4d027b'] = 'Platba v hotovosti';
+$_MODULE['<{cashonpickup}prestashop>payment_59ce9b867da2f7de0caabd7604aaf8fa'] = 'Za zboží zaplatíte při vyzvednutí';
+$_MODULE['<{cashonpickup}prestashop>payment_59ce9b867da2f7de0caabd7604aaf8fa'] = 'Za zboží zaplatíte při vyzvednutí';
